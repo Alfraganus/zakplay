d79a6d37e38d3932e715051a2886041698d357e1
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-04-27 15:22:03 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+Route::get('/docs', [SwaggerController::class, 'index']);
+Route::get('/docs/{any}', [SwaggerController::class, 'index'])->where('any', '.*');
 require base_path('app/Modules/test/Routes.php');

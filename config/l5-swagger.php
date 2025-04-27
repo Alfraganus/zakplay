6c133f7363a4954460fb5c9bdc6a805a8e65b83a
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-04-27 17:48:05 +0500

    Update l5-swagger.php

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -54,7 +54,7 @@ return [
             /*
              * Route for accessing parsed swagger annotations.
              */
-            'docs' => 'asset',
+            'docs' => 'public',
 
             /*
              * Route for Oauth2 authentication callback.
@@ -81,7 +81,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => 'docs',
 
             /*
              * Absolute path to directory where to export views

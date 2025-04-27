e6b509f7ef8983a934c180a6f05b42587528dbcc
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-04-27 15:19:27 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::any('/docs/{any}', function ($any) {
+    return redirect('/api/documentation');
+})->where('any', '.*');
+
 Route::get('/', function () {
     return view('welcome');
 });

25cd07458e5e6eae06c941d142630dd29de27576
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-05-09 20:55:29 +0500

    initia upload

--- app/Modules/test/models/Ads.php
+++ app/Modules/test/models/Ads.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 class Ads extends Model
 {
     protected $table = 'ads';
-    public $timestamps = false;
     protected $fillable = [
         'department_id',
         'title',

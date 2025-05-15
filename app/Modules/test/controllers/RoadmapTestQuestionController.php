74c400569854dc478022a28a088eace5d5a7f2a0
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-05-15 19:08:52 +0500

    initia upload

--- app/Modules/test/controllers/RoadmapTestQuestionController.php
+++ app/Modules/test/controllers/RoadmapTestQuestionController.php
@@ -34,12 +34,9 @@ class RoadmapTestQuestionController extends Controller
 
         $priority = 1;
         foreach ($departments as $department) {
-            if (is_null($department->priority_number)) {
                 $department->priority_number = $priority++;
                 $department->save();
-            } else {
-                $priority = max($priority, $department->priority_number + 1);
-            }
+
         }
 
         if (!$currentDepartment) {

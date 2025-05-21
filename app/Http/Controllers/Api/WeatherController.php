1fef72183f46c6130cb1406ad71743393da759dd
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-05-21 17:00:09 +0500

    initia upload

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Service\WeatherService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
@@ -21,11 +22,38 @@ class WeatherController extends Controller
         return response()->json($this->weather->getCurrentWeather($city));
     }
 
+
     public function forecast(Request $request)
     {
         $city = $request->input('city', 'Tashkent');
         $days = $request->input('days', 5);
-        return response()->json($this->weather->getForecast($city, $days));
+
+        $cacheKey = 'weather:' . strtolower($city) . ':' . now()->toDateString();
+
+        $result = Cache::remember($cacheKey, now()->addDay(), function () use ($city, $days) {
+            $forecast = $this->weather->getForecast($city, $days);
+
+            $forecastData = array_map(function ($day) {
+                return [
+                    'date' => $day['date'],
+                    'mintemp_c' => $day['day']['mintemp_c'],
+                    'maxtemp_c' => $day['day']['maxtemp_c'],
+                    'avgtemp_c' => $day['day']['avgtemp_c'],
+                    'condition' => $day['day']['condition'],
+                ];
+            }, $forecast['forecast']['forecastday']);
+
+            return [
+                'location' => $forecast['location'],
+                'current' => [
+                    'temp_c' => $forecast['current']['temp_c'],
+                    'condition' => $forecast['current']['condition'],
+                    'forecast' => $forecastData,
+                ],
+            ];
+        });
+
+        return response()->json($result);
     }
 
     public function search(Request $request)
